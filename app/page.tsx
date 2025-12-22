36406242961f63ffa7d10444dd73a68bfa3e78b3
Mark Christian Mallari <mallariftw01@gmail.com>
2025-12-22 21:18:10 +0800

    refactor: Change pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import Link from "next/link";
 
-export default function Home() {
+const BlogsPage = () => {
   return (
-    <nav>
-      {/* Prefetched when the link is hovered or enters the viewport */}
-      <Link href="/blog">Blog</Link>
-      {/* No prefetching */}
-      <a href="/contact">Contact</a>
-    </nav>
-  );
+    <div>
+      <h1>This is blog page.</h1>
+    </div>
+  )
 }
+
+export default BlogsPage
+
