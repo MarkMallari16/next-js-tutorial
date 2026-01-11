9d7ceed95368fd7716282dfefaa00b87a043a6ba
Mark Christian Mallari <mallariftw01@gmail.com>
2026-01-11 21:17:09 +0800

    feat: Practicing use client

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,31 @@
-import Link from 'next/link'
-import React from 'react'
 
-const UsersPage = () => {
+import Link from 'next/link'
+
+const UsersPage = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+
+    /*
+    Server Components = data + layout
+Client Components = interactivity
+    */
     return (
         <div className='min-h-screen grid place-items-center'>
             <div>
                 <h1 className='text-5xl font-black'>Users Page</h1>
-                <Link href="/blog" className="rounded-sm bg-white text-black p-2 font-medium">Go to Blog</Link>
+                <div className='mt-2'>
+                    {
+                        users?.map((user => (
+                            <li key={user.id}>
+                                <p>{user.email}</p>
+                            </li>
+                        )))
+                    }
+                </div>
+
+                <div className='mt-4'>
+                    <Link href="/blog" className="rounded-sm bg-white text-black p-2 font-medium">Go to Blog</Link>
+                </div>
             </div>
         </div>
     )
