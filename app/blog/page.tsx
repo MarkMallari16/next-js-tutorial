af9e3704b5d70d2c45681b035916414ceb50116e
Mark Christian Mallari <mallariftw01@gmail.com>
2026-01-14 21:52:35 +0800

    feat: Adding blogs fake data

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,15 +1,38 @@
 import Link from "next/link"
 import FirstButton from "../ui/button/FirstButton"
 
-const BlogPage = () => {
+
+interface Blog {
+    id: number;
+    title: string;
+    body: string;
+}
+const BlogPage = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const blogs: Blog[] = await res.json();
+
     return (
         <div className='min-h-screen grid place-items-center'>
             <div>
                 <h1 className='text-5xl font-black mb-2'>Blog Page</h1>
-                <Link href="/users" className="rounded-sm bg-white text-black p-2 font-medium">Go to Users</Link>
+                <div >
+                    {
+                        blogs?.slice(0,10).map((blog) => (
+                            <div key={blog.id} className="border mb-4 p-2 rounded-sm">
+                                <h1 className="text-2xl font-bold">{blog.title}</h1>
+                                <p className="text-gray-300">{blog.body}</p>
+                            </div>
+                        ))
+                    }
+                </div>
 
-                <FirstButton likes={10} />
+                <div className="mt-5 flex gap-2">
+                    <Link href="/users" className="rounded-sm bg-white text-black p-2 font-medium">Go to Users</Link>
+                    <FirstButton likes={10} />
+                </div>
+
             </div>
+
         </div>
     )
 }
