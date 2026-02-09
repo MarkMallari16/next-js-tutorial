be5518e739f44a4045030d2f518c007336acc88d
Mark Christian Mallari <mallariftw01@gmail.com>
2026-02-09 22:59:09 +0800

    feat: Add Line chart

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,6 +1,8 @@
+import LineChart from "./line-chart"
 import SalesChart from "./sales-chart"
 
 const page = () => {
+
     const chartData = [
         { month: "January", desktop: 186, mobile: 80, laptop: 120 },
         { month: "February", desktop: 305, mobile: 200, laptop: 120 },
@@ -10,10 +12,24 @@ const page = () => {
         { month: "June", desktop: 214, mobile: 140, laptop: 120 },
     ]
 
+    const lineChartData = [
+        { month: "January", desktop: 186 },
+        { month: "February", desktop: 305 },
+        { month: "March", desktop: 237 },
+        { month: "April", desktop: 73 },
+        { month: "May", desktop: 209 },
+        { month: "June", desktop: 214 },
+    ]
 
     return (
-        <div>
-            <SalesChart data={chartData} />
+        <div className="container mx-auto">
+            <h1 className="text-4xl font-bold text-center pt-2">Basic Chart Analytics</h1>
+            <div>
+                <SalesChart data={chartData} />
+            </div>
+            <div>
+                <LineChart data={lineChartData} />
+            </div>
         </div>
     )
 }
