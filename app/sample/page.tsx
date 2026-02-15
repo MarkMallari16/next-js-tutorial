3b581f9362b28d88063ce662eab1fc3e87404536
Mark Christian Mallari <mallariftw01@gmail.com>
2026-02-15 10:22:24 +0800

    feat: Add drawer component

--- app/sample/page.tsx
+++ app/sample/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button"
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer"
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
 
 const page = () => {
+    const DRAWER_SIDES = ["top", "bottom", "left", "right"] as const;
+
+
+
     return (
-        <div className="flex justify-center items-center min-h-screen">
+        <div className="flex flex-col gap-4 justify-center items-center min-h-screen">
             <ResizablePanelGroup
                 orientation="horizontal"
                 className="max-w-lg rounded-lg border "
@@ -29,6 +35,29 @@ const page = () => {
                     </ResizablePanelGroup>
                 </ResizablePanel>
             </ResizablePanelGroup>
+
+
+            <div>
+                {DRAWER_SIDES.map((side) => (
+                    <Drawer key={side} direction={side === "bottom" ? undefined : (side as "top" | "left" | "right")}>
+                        <DrawerTrigger asChild>
+                            <Button variant='outline' className="cursor-pointer">{side}</Button>
+                        </DrawerTrigger>
+                        <DrawerContent>
+                            <DrawerHeader>
+                                <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                                <DrawerDescription>This action cannot be undone.</DrawerDescription>
+                            </DrawerHeader>
+                            <DrawerFooter>
+                                <Button>Submit</Button>
+                                <DrawerClose>
+                                    <Button variant="outline">Cancel</Button>
+                                </DrawerClose>
+                            </DrawerFooter>
+                        </DrawerContent>
+                    </Drawer>
+                ))}
+            </div>
         </div>
     )
 }
