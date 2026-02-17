ee80e624bb1855d54a97b0732b8a5313bc8bdda4
Mark Christian Mallari <mallariftw01@gmail.com>
2026-02-17 09:18:03 +0800

    feat: Add badge

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import BasicInput from "@/components/shared/BasicInput"
 import BasicSonner from "@/components/shared/BasicSonner"
 import Navbar from "@/components/shared/Navbar"
 import OtpInput from "@/components/shared/OtpInput"
+import { Badge } from "@/components/ui/badge"
 import { Label } from "@/components/ui/label"
 
 const Home = () => {
@@ -13,26 +14,28 @@ const Home = () => {
 
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <h1 className="text-2xl font-black">Home page</h1>
       <Label>Hello</Label>
       <OtpInput />
-
       <Label>Basic Input</Label>
       <BasicInput className="max-w-xs" />
-
       <div className="mt-4">
         <BasicSonner />
       </div>
-
-      <div className="mt-4">
-        <div>
-          <h1 className="text-3xl font-bold">Frequently Asked Question?</h1>
-          <BasicAccordion />
-        </div>
+      <div className="m-4">
+        <h1 className="text-3xl font-bold ">Frequently Asked Question?</h1>
+        <BasicAccordion />
       </div>
+      <AddDialog />
 
-      <AddDialog/>
+      <div className="flex gap-1 m-4">
+        <Badge variant="outline">Outline</Badge>
+        <Badge variant="secondary">Secondary</Badge>
+        <Badge variant="link">Link</Badge>
+        <Badge variant="destructive">Danger</Badge>
+
+      </div>
     </div>
   )
 }
