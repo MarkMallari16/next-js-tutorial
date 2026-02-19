6f9388882e5783202008066d537777f410598fef
Mark Christian Mallari <mallariftw01@gmail.com>
2026-02-19 21:33:36 +0800

    feat: Add Hover Card

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import BasicSonner from "@/components/shared/BasicSonner"
 import Navbar from "@/components/shared/Navbar"
 import OtpInput from "@/components/shared/OtpInput"
 import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card"
 import { Label } from "@/components/ui/label"
 
 const Home = () => {
@@ -34,7 +36,17 @@ const Home = () => {
         <Badge variant="secondary">Secondary</Badge>
         <Badge variant="link">Link</Badge>
         <Badge variant="destructive">Danger</Badge>
+      </div>
 
+      <div>
+        <HoverCard openDelay={1} closeDelay={1}>
+          <HoverCardTrigger asChild>
+            <Button>Hover Me</Button>
+          </HoverCardTrigger>
+          <HoverCardContent>
+            Pogi ni Mark Mallari
+          </HoverCardContent>
+        </HoverCard>
       </div>
     </div>
   )
