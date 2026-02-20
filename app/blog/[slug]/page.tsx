51895c46a452af8aef23375b727abd4d469cfba3
Mark Christian Mallari <mallariftw01@gmail.com>
2026-02-20 20:46:46 +0800

    Add basic file based routing

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Sample React Slug File Base routing</div>
+  )
+}
+
+export default page
---
+++ app/blog/post/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+const page = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-black">This is post page</h1>
+            <Button>Sample Button</Button>
+
+        </div>
+    )
+}
+
+export default page
